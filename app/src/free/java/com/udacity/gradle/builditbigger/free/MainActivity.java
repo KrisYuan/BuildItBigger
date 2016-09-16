e0fafbc148f3958a6009a7cbdf13fd05ef2f0878
vanpersie0806 <13611995216@139.com>
2016-09-16 10:26:33 +0800

    Add product flavors

---
+++ app/src/free/java/com/udacity/gradle/builditbigger/free/MainActivity.java
@@ -0,0 +1,45 @@
+package com.udacity.gradle.builditbigger.free;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.udacity.gradle.builditbigger.JokeAsyncTask;
+import com.udacity.gradle.builditbigger.R;
+import com.udacity.gradle.jokedisplay.JokeActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    public void tellJoke(View view) {
+        new JokeAsyncTask(){
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Intent intent = new Intent(MainActivity.this, JokeActivity.class);
+                intent.putExtra(JokeActivity.JOKE_TAG, s);
+                startActivity(intent);
+            }
+        }.execute();
+
+    }
+
+}
